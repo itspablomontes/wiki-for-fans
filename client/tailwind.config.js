8d0c7f880386d107d8dc0b02d62d2069a38d3637
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2024-12-14 15:31:03 -0300

    chore: add pages and components folders

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
