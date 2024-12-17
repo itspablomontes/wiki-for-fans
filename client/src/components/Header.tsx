cfeb0468f99fb18a631cf9e09add629c2cc4ddef
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2024-12-17 18:46:31 -0300

    feat: add mobile drag down menu

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,37 +1,91 @@
+import { motion } from "motion/react";
+import { useState } from "react";
+
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="flex justify-between items-center py-6 px-8 md:px-32 bg-black drop-shadow-md">
-      <a href="">
+      <motion.a
+        href=""
+        whileTap={{ scale: 0.9 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+      >
         <img
           src="src/assets/wikilogo.svg"
           alt="wiki logo"
-          className="w-28 md:w-32 hover:scale-105 transition-all"
+          className="w-24 md:w-32 hover:scale-105 transition-all"
         />
-      </a>
-      <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
-        <li className="p-3 hover:bg-gray-900 rounded-md transition-all cursor-pointer">
+      </motion.a>
+      <motion.ul
+        className="hidden xl:flex items-center gap-12 font-semibold text-base"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+      >
+        <motion.li
+          className="p-3 hover:bg-gray-900 rounded-md transition-all cursor-pointer"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.5 }}
+        >
           Home
-        </li>
-        <li className="p-3 hover:bg-gray-900 rounded-md transition-all cursor-pointer">
+        </motion.li>
+        <motion.li
+          className="p-3 hover:bg-gray-900 rounded-md transition-all cursor-pointer"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.5 }}
+        >
           Characters
-        </li>
-        <li className="p-3 hover:bg-gray-900 rounded-md transition-all cursor-pointer">
+        </motion.li>
+        <motion.li
+          className="p-3 hover:bg-gray-900 rounded-md transition-all cursor-pointer"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.5 }}
+        >
           Houses
-        </li>
-      </ul>
-      <div className="relative hidden md:flex items-center justify-center gap-3 ">
+        </motion.li>
+      </motion.ul>
+      <motion.form
+        className="hidden relative md:flex items-center justify-center gap-3 "
+        whileHover={{ scale: 1.05 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+      >
         <img
           src="src/assets/search.svg"
           alt="search logo"
           className="w-7 absolute left-3"
         />
-        <input
-          type="text"
+        <motion.input
+          type="search"
+          name="search"
+          id="search"
           placeholder="Search..."
-          className="py-2 pl-10 border-2 border-gray-900 bg-black text-white rounded-2xl focus:bg"
+          className="py-2 pl-10 border-2 border-gray-900 bg-black text-white rounded-2xl"
         />
-      </div>
-      <i className="bx bx-menu text-4xl xl:hidden block cursor pointer"></i>
+      </motion.form>
+      <motion.i
+        className="bx bx-menu text-4xl xl:hidden block cursor pointer"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        whileTap={{ scale: 0.9 }}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      />
+      <motion.div
+        className={`absolute xl:hidden top-24 left-0 w-full  bg-zinc-900 flex flex-col items-center gap-6 font-semibold text-lg ${
+          isMenuOpen ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <motion.li className="list-none w-full text-center p-4 hover:bg-gray-900 border-y-2 border-x-white cursor-pointer">
+          Home
+        </motion.li>
+        <motion.li className="list-none w-full text-center p-4 hover:bg-gray-900 border-y-2 border-x-white cursor-pointer">
+          Characters
+        </motion.li>
+        <motion.li className="list-none w-full text-center p-4 hover:bg-gray-900 border-y-2 border-x-white cursor-pointer">
+          Houses
+        </motion.li>
+      </motion.div>
     </header>
   );
 };
