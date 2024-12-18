4286e99d3980a493bdfec48bc1df9933daf89f9e
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2024-12-18 17:36:28 -0300

    feat: add mobile search bar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
     <header className="flex justify-between items-center py-6 px-8 md:px-32 bg-black drop-shadow-md">
@@ -64,13 +65,24 @@ const Header = () => {
           className="py-2 pl-10 border-2 border-gray-900 bg-black text-white rounded-2xl"
         />
       </motion.form>
-      <motion.i
-        className="bx bx-menu text-4xl xl:hidden block cursor pointer"
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        whileTap={{ scale: 0.9 }}
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      />
+      <motion.div className="flex flex-row items-center gap-4">
+        <motion.i
+          className="bx bx-search text-2xl md:hidden font-bold"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={() => {
+            setIsSearchOpen(!isSearchOpen);
+          }}
+        ></motion.i>
+        <motion.i
+          className="bx bx-menu text-4xl xl:hidden block cursor pointer"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+      </motion.div>
       <motion.div
         className={`absolute xl:hidden top-24 left-0 w-full  bg-zinc-900 flex flex-col items-center gap-6 font-semibold text-lg ${
           isMenuOpen ? "opacity-100" : "opacity-0"
@@ -89,6 +101,22 @@ const Header = () => {
           Houses
         </motion.li>
       </motion.div>
+      <motion.form
+        className={`absolute xl:hidden top-24 left-0 flex justify-center ${
+          isSearchOpen ? "opacity-1" : "opacity-0"
+        }`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: isSearchOpen ? 1 : 0 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      >
+        <motion.input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search..."
+          className="py-2 px-2 border-2 border-gray-900 bg-black text-white rounded-2xl w-4/5"
+        />
+      </motion.form>
     </header>
   );
 };
