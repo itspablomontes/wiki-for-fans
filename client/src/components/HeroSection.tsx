4bd7e139490621450b84df76d39defe727f94509
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2024-12-26 22:36:17 -0300

    feat: add animations and style to HeroSection

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -2,10 +2,23 @@ import { motion } from "motion/react";
 
 const HeroSection = () => {
   return (
-    <div className=" px-8 w-auto py-96 flex flex-col justify-center bg-hero-background bg-center text-2xl font-bold text-center bg-cover md:bg-[center_top]">
-      <div>Discover the lore of Westeros</div>
-      <motion.a href="" className="cursor-pointer">
-        <motion.i className="bx bx-down-arrow"></motion.i>
+    <div className=" px-8 w-auto py-96 flex flex-col gap-5 justify-center bg-hero-background bg-center font-bold text-center bg-cover md:bg-[center_top]">
+      <motion.div
+        className="md:text-4xl font-custom text-xl"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+      >
+        DISCOVER THE LORE OF WESTEROS
+      </motion.div>
+      <motion.a
+        href="#cards"
+        className="cursor-pointer flex justify-center px-6 py-3 w-min bg-[#FE0000] rounded-lg self-center"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        Explore
       </motion.a>
     </div>
   );
