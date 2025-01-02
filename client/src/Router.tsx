03069db6b8203dcfee2144f09f9a0930ec2029a6
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-02 15:50:08 -0300

    feat: add Character page

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import Character from "./routes/Character";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/character" element={<Character />} />
       </Route>
     </Routes>
   );
