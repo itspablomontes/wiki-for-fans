aed7dde9cf1b5bf3e0cf0ad8eb98033686807c01
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-03 12:08:22 -0300

    feat: add animations to characters list

--- client/src/components/CharacterListItem.tsx
+++ client/src/components/CharacterListItem.tsx
@@ -1,20 +1,30 @@
 import { Link } from "react-router-dom";
-
+import { motion } from "motion/react";
 const CharacterListItem = () => {
   return (
     <Link
       to="/character"
       className="flex flex-col justify-center items-center gap-3 "
     >
-      <div className="p-1 hover:bg-gradient-to-r from-red-600  to-orange-700 rounded-full">
+      <motion.div
+        className="p-1 hover:bg-gradient-to-r from-red-600  to-orange-700 rounded-full"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+      >
         <img
           src="src/assets/jon-small-icon.webp"
           alt="jon snow"
           className="max-w-64 rounded-full"
         />
-      </div>
+      </motion.div>
 
-      <div className="font-custom text-2xl ">Jon Snow</div>
+      <motion.div
+        className="font-custom text-2xl "
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+      >
+        Jon Snow
+      </motion.div>
     </Link>
   );
 };
