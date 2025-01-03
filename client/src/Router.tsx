b7b10a1b9c0959a005bc1f17fe0f505728628c2d
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-03 13:13:13 -0300

    feat: add houses list page

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -3,6 +3,7 @@ import Home from "./routes/Home";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import Character from "./routes/Character";
 import CharactersList from "./routes/CharactersList";
+import HousesList from "./routes/HousesList";
 
 export function Router() {
   return (
@@ -11,6 +12,7 @@ export function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/character" element={<Character />} />
         <Route path="characters-list" element={<CharactersList />} />
+        <Route path="houses-list" element={<HousesList />} />
       </Route>
     </Routes>
   );
