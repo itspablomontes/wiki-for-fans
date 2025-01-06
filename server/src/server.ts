cd05f70a1d0ca64eb1062c8d463c6d71d72504b3
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-06 14:51:56 -0300

    refac: use variables in server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 
-app.listen(8080, () => {
-  console.log("Server Running on Port 8080");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
 });
