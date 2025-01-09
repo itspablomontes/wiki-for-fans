ced15d377cad0c07a230aa7fa9b2ee20e6c7d655
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-09 20:34:22 -0300

    feat: add character removal route

--- server/src/routes/charactersRoutes.ts
+++ server/src/routes/charactersRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getCharacters,
   addCharacter,
+  removeCharacterById,
 } from "../controllers/charactersController";
 
 const charactersRoutes = Router();
 
 charactersRoutes.get("/", getCharacters);
-charactersRoutes.post("/create", addCharacter);
+charactersRoutes.post("/add-character", addCharacter);
+charactersRoutes.delete("/remove-character", removeCharacterById);
 
 export default charactersRoutes;
