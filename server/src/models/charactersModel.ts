d7f56a4b37179c028ef2c5eada7504946df08ebd
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-09 19:13:07 -0300

    feat: add createCharacter to Character Model

--- server/src/models/charactersModel.ts
+++ server/src/models/charactersModel.ts
@@ -1,6 +1,60 @@
+import { error } from "console";
 import pool from "../db";
+
+export interface Character {
+  id?: number;
+  name: string;
+  born: string;
+  died: string;
+  house: string;
+  titles: string[];
+  affiliations: string[];
+  phrase: string;
+  description: string[];
+  profile_image_url: string;
+  created_at: Date;
+  updated_at: Date;
+}
 
 export const getAllCharacters = async () => {
   const { rows } = await pool.query("SELECT * FROM characters");
   return rows;
+};
+
+export const createCharacter = async (
+  character: Omit<Character, "id" | "created_at" | "updated_at">
+): Promise<Character> => {
+  const query = `
+      INSERT INTO characters (
+      name,
+      born,
+      died,
+      house,
+      titles,
+      affiliations,
+      phrase,
+      description,
+      profile_image_url
+      )
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;
+    `;
+
+  try {
+    const values = [
+      character.name,
+      character.born,
+      character.died,
+      character.house,
+      character.titles,
+      character.affiliations,
+      character.phrase,
+      character.description,
+      character.profile_image_url,
+    ];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    console.error("Database error:", error);
+    throw error;
+  }
 };
