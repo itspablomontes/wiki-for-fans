b0947ec5403b22c4536e96e8b18934aad45f5183
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-11 10:50:45 -0300

    feat: add findUserById route

--- server/src/routes/charactersRoutes.ts
+++ server/src/routes/charactersRoutes.ts
@@ -3,11 +3,13 @@ import {
   getCharacters,
   addCharacter,
   removeCharacterById,
+  findCharacterById,
 } from "../controllers/charactersController";
 
 const charactersRoutes = Router();
 
 charactersRoutes.get("/", getCharacters);
+charactersRoutes.get("/find-character", findCharacterById);
 charactersRoutes.post("/add-character", addCharacter);
 charactersRoutes.delete("/remove-character", removeCharacterById);
 
