abfb66a4e66bbc0f1dc5455fc3b20b864fe46e0e
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-12 18:11:22 -0300

    feat: add update character route

--- server/src/routes/charactersRoutes.ts
+++ server/src/routes/charactersRoutes.ts
@@ -4,6 +4,7 @@ import {
   addCharacter,
   removeCharacterById,
   findCharacterById,
+  updateCharacterById,
 } from "../controllers/charactersController";
 
 const charactersRoutes = Router();
@@ -12,5 +13,6 @@ charactersRoutes.get("/", getCharacters);
 charactersRoutes.get("/find-character/:id", findCharacterById);
 charactersRoutes.post("/add-character", addCharacter);
 charactersRoutes.delete("/remove-character", removeCharacterById);
+charactersRoutes.put("/update-character/:id", updateCharacterById);
 
 export default charactersRoutes;
