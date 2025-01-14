ca6edbac8dc5ccac337f16b5a847338b103cec6b
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-14 19:27:21 -0300

    feat: add addHouse controller

--- server/src/controllers/housesController.ts
+++ server/src/controllers/housesController.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 
-import { getAllHouses, getHouseById } from "../models/housesModel";
+import {
+  createHouse,
+  getAllHouses,
+  getHouseById,
+  House,
+} from "../models/housesModel";
 
 export const getHouses = async (req: Request, res: Response) => {
   try {
@@ -30,11 +35,39 @@ export const findHouseById = async (req: Request, res: Response) => {
     }
     res.status(200).json({ message: "Character Found!", data: targetHouse });
   } catch (error) {
+    res.status(500).json({
+      error:
+        error instanceof Error ? error.message : "An unknown error occured",
+    });
+  }
+};
+
+export const addHouse = async (req: Request, res: Response) => {
+  try {
+    const houseData: Omit<House, "id" | "created_at" | "updated_at"> = req.body;
+
+    if (
+      !houseData.name ||
+      !houseData.coat_of_arms ||
+      !houseData.words ||
+      !houseData.seat ||
+      !houseData.region ||
+      !houseData.head ||
+      !houseData.notable_characters ||
+      !houseData.description ||
+      !houseData.house_banner_url
+    ) {
+      res.status(400).json({ message: "All fields are required" });
+      return;
+    }
+    const newHouse = await createHouse(houseData);
+    res.status(201).json(newHouse);
+  } catch (error) {
     res
       .status(500)
       .json({
         error:
-          error instanceof Error ? error.message : "An unknown error occured",
+          error instanceof Error ? error.message : "An unknown error ocurred",
       });
   }
 };
--- server/src/routes/houses.routes.ts
+++ server/src/routes/houses.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { findHouseById, getHouses } from "../controllers/housesController";
+import {
+  addHouse,
+  findHouseById,
+  getHouses,
+} from "../controllers/housesController";
 
 export const housesRoutes = Router();
 
 housesRoutes.get("/", getHouses);
 housesRoutes.get("/find-house/:id", findHouseById);
+housesRoutes.post("/add-house", addHouse);
