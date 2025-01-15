b2f00ddd35735a5083f2b57b341b53cff5db111d
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-15 16:42:23 -0300

    feat: add api integration to CharacterCard page

--- client/src/routes/CharactersList.tsx
+++ client/src/routes/CharactersList.tsx
@@ -1,7 +1,38 @@
 import CharacterListItem from "../components/CharacterListItem";
 import { motion } from "motion/react";
+import api from "../services/api";
+import { useEffect, useState } from "react";
+
+export interface Character {
+  id?: number;
+  name: string;
+  born: string;
+  died: string;
+  house: string;
+  titles: string[];
+  affiliations: string[];
+  phrase: string;
+  description: string[];
+  profile_image_url: string;
+  created_at: Date;
+  updated_at: Date;
+}
 
 const CharactersList = () => {
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  const getCharacters = async () => {
+    try {
+      const response = await api.get<Character[]>("/characters");
+      setCharacters(response.data);
+    } catch (error) {
+      console.error("Error fetching characters: ", error);
+    }
+  };
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center py-8 px-6 gap-9">
       <motion.div
@@ -12,14 +43,13 @@ const CharactersList = () => {
         CHARACTERS
       </motion.div>
       <div className="grid md:grid-cols-[1fr_1fr] xl:grid-cols-[1fr_1fr_1fr_1fr] gap-6 wrap">
-        <CharacterListItem />
-        <CharacterListItem />
-        <CharacterListItem />
-        <CharacterListItem />
-        <CharacterListItem />
-        <CharacterListItem />
-        <CharacterListItem />
-        <CharacterListItem />
+        {characters.map((character) => (
+          <CharacterListItem
+            key={character.id}
+            name={character.name}
+            profile_image_url={character.profile_image_url}
+          />
+        ))}
       </div>
     </div>
   );
