5b0f8e20f84886e6f3e990f8a3c6507ff5a26fd6
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-15 15:50:29 -0300

    fix: add cors to back end

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import router from "./router";
+import cors from "cors";
+import path from "path";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
+
+app.use("/images", express.static(path.join(__dirname, "src/images")));
 
 export default app;
