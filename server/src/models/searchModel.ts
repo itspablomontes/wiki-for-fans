0693673b4834d57365530c97a5867100d6a2719f
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-19 17:22:38 -0300

    feat: add search model function

---
+++ server/src/models/searchModel.ts
@@ -0,0 +1,28 @@
+import pool from "../db";
+
+export const searchAll = async (query: string) => {
+  try {
+    const charactersResult = await pool.query(
+      `SELECT id, name, born, died, house, affiliations, phrase, 
+            description, profile_image_url
+            FROM characters
+            WHERE name ILIKE $1 or house ILIKE $1`,
+      [`%${query}%`]
+    );
+    const housesResult = await pool.query(
+      `SELECT id, name, coat_of_arms, words, seat, region, head, notable_characters,
+       description, house_banner_url 
+      FROM houses
+      WHERE name ILIKE $1`,
+      [`%${query}%`]
+    );
+
+    return {
+      characters: charactersResult.rows,
+      houses: housesResult.rows,
+    };
+  } catch (error) {
+    console.error("Database error: ", error);
+    throw error;
+  }
+};
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { charactersRoutes } from "./routes/characters.routes";
 import { housesRoutes } from "./routes/houses.routes";
+import { searchController } from "./controllers/searchController";
 const router = express.Router();
 
 router.use("/characters", charactersRoutes);
 router.use("/houses", housesRoutes);
+router.get("/search/", searchController);
 
 export default router;
